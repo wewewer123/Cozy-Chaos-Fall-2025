0145c3c5945883fa5c0d391c4b2cf8a07d3b8668
Henry <65220433+HenRMJ@users.noreply.github.com>
2025-10-26 09:32:58 -0700

    Fix Spawn System & Add Tree Variation
    I refactored the way the spawn system work to work off of spawners now instead of each element handling itself. And I also moved the way that the player moves and removed the camera from the player to have a static camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,26 +1,36 @@
 extends CharacterBody3D
+class_name PlayerObject
 
-var max_lanes: int
-var curr_lane: int
 @export var lives = 5
 @export var leaf = 0
 @export var max_velocity = 10
+@export var move_speed: float = 15.0  
 
+var max_lanes: int
+var curr_lane: int
+var lane_width: float = 25.0
+
+var target_x: float
+var spawner: Spawner
+
+func init(s: Spawner) -> void:
+	spawner = s 
+	max_lanes = spawner.get_lane_count()
+	curr_lane = max_lanes / 2
+	target_x = spawner.get_lane_position(curr_lane).x
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("left"):
 		consume_movement(-1)
 	if Input.is_action_just_pressed("right"):
 		consume_movement(1)
-	print(lives)
 
-func _physics_process(_delta: float) -> void:
-	# simply teleporting for now as actually moving is too slow and blocks the input 
-	position += velocity
-	velocity.x = 0
+func _physics_process(delta: float) -> void:
+	# Smoothly move toward the target lane’s X
+	position.x = lerp(position.x, target_x, move_speed * delta)
 
 func consume_movement(direction: int) -> void:
-	var new_lane = curr_lane+sign(direction)
-	if velocity.x == 0 and new_lane >= 0 and new_lane < max_lanes:
-			velocity.x = sign(direction) * max_velocity
-			curr_lane = new_lane
+	var new_lane = curr_lane + sign(direction)
+	if new_lane >= 0 and new_lane < max_lanes:
+		curr_lane = new_lane
+		target_x = spawner.get_lane_position(curr_lane).x
--- scripts/tree.gd
+++ scripts/tree.gd
@@ -1,18 +1,40 @@
-extends Area3D
+extends CollisionObject
 
-signal player_collision
+enum TreeType { SUMMER, SEMI_AUTUMN, AUTUMN }
+
+@export var summer_textures: Array[Texture2D]
+@export var semi_autumn_textures: Array[Texture2D]
+@export var autumn_textures: Array[Texture2D]
+
+@export var treeType: TreeType
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var rng:= RandomNumberGenerator.new()
+	type = CollisionType.TREE
+	replace_sprites()
+
+func replace_sprites() -> void:
+	var textures: Array[Texture2D]
 	
-	lane_spawn(rng.randi_range(-1, 1))
+	match type:
+		TreeType.SUMMER:
+			textures = summer_textures
+		TreeType.SEMI_AUTUMN:
+			textures = semi_autumn_textures
+		TreeType.AUTUMN:
+			textures = autumn_textures
+	
+	if textures.is_empty():
+		push_warning("No textures set for this tree type")
+		return
+	
+	var rng := RandomNumberGenerator.new()
+	rng.randomize()
+	var index := rng.randi_range(0, textures.size() - 1)
+	
+	for child in get_children():
+		if child is Sprite3D:
+			child.texture = textures[index]
 
-func _process(delta):
-	position += Vector3(0, 0, 1.0)*20*delta
-
-func lane_spawn(direction: int) -> void:
-	position = Vector3((sign(direction)*10), 0, -20)
-
-func _on_body_entered(body: Node3D) -> void:
-	emit_signal("player_collision")
+func apply_effect(player: Node3D) -> void:
+	player.lives -= 1
