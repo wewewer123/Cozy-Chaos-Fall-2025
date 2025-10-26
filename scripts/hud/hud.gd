265e044170b4749544e447b1b4cf0f06ee3e440a
tomko-code <108701315+Tomko-Code@users.noreply.github.com>
2025-10-26 18:58:01 +0100

    hud (#6)

---
+++ scripts/hud/hud.gd
@@ -0,0 +1,42 @@
+extends CanvasLayer
+class_name HUD
+
+@export var hearth_container:HBoxContainer
+@export var empty_hearth_container:HBoxContainer
+
+var hear_res:PackedScene = load("res://scripts/hud/hearth_full.tscn")
+var empty_hear_res:PackedScene = load("res://scripts/hud/hearth_empty.tscn")
+
+func _ready() -> void:
+	pass
+
+func init() -> void:
+	for x in range(GameManager.player.max_health):
+		add_heart()
+
+func on_player_leaf_collision() -> void:
+	pass
+
+func on_player_health_changed(change:int) -> void:
+	print(change)
+	if change > 0:
+		add_heart()
+	else:
+		remove_hearth()
+
+func on_player_leaf_changed(value:int) -> void:
+	$Control/MarginContainer2/HBoxContainer/Label.text = str(GameManager.player.leaf)
+
+func add_heart() -> void:
+	if empty_hearth_container.get_child_count() > 0:
+		empty_hearth_container.get_child(0).queue_free()
+		
+	hearth_container.add_child(hear_res.instantiate())
+	
+func remove_hearth() -> void:
+	if hearth_container.get_child_count() > 0:
+		hearth_container.get_child(0).queue_free()
+	empty_hearth_container.add_child(empty_hear_res.instantiate())
+
+func on_player_death() -> void:
+	hide()
