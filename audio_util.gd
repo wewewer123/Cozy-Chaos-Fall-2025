1779920e3638b5ab7776b62f12416c7d69366a7a
Tick <62646477+Tick-git@users.noreply.github.com>
2025-11-17 18:16:02 +0100

    Fixes Returning To Main Menu Via Escape

--- audio_util.gd
+++ audio_util.gd
@@ -1,5 +1,5 @@
 extends Node
 
 func wait_audio_finished(audio_source:AudioStreamPlayer2D) -> void:
-	while audio_source.playing:
+	while audio_source != null and audio_source.playing:
 		await get_tree().process_frame
