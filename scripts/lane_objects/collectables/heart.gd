75ef5792627318b4cc6940d0003fff6ac7768379
Tick <62646477+Tick-git@users.noreply.github.com>
2025-11-19 23:57:46 +0100

    Makes Heart Appear Less Static Visually

--- scripts/lane_objects/collectables/heart.gd
+++ scripts/lane_objects/collectables/heart.gd
@@ -1,8 +1,25 @@
 extends CollisionObject
 
-# Called when the node enters the scene tree for the first time.
+var dynamic_scale:float
+var frequency:float = 0.2
+var multiplier:float = -1
+var min_scale = 0.95
+var max_scale = 1
+
 func _ready():
 	type = CollisionType.HEART
+	
+func _process(delta: float) -> void:
+	dynamic_scale += delta * frequency * multiplier
+	
+	scale = Vector3(dynamic_scale, dynamic_scale, dynamic_scale)
+	
+	if dynamic_scale < min_scale:
+		dynamic_scale = min_scale
+		multiplier *= -1
+	elif dynamic_scale > max_scale:
+		dynamic_scale = max_scale
+		multiplier *= -1
 
 func apply_effect(player: Node3D) -> void:
 	queue_free()
