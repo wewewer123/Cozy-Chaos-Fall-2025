b02388340d2d9fa5fe0342b6453875f6bcc72522
Tick <62646477+Tick-git@users.noreply.github.com>
2025-11-19 14:11:00 +0100

    Fixes Moving Object Despawning

--- scripts/world/node_mover.gd
+++ scripts/world/node_mover.gd
@@ -4,14 +4,10 @@ const DESPAWN_Y_VALUE: float = 70
 
 var parent: Node3D  
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	parent = get_parent()
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	parent.position += Vector3(0, 0, 1.0)*20*delta
-	if parent.position.z > DESPAWN_Y_VALUE:
-		queue_free()
+	parent.global_position += Vector3(0, 0, 1.0) * 20 * delta
+	if parent.global_position.z > DESPAWN_Y_VALUE:
+		parent.queue_free()
