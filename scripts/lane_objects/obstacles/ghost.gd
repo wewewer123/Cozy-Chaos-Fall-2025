a9787c03ee09f38eb8ed092dc45254aa865cc70c
Tick <62646477+Tick-git@users.noreply.github.com>
2025-11-19 23:40:27 +0100

    Makes Ghost Obstacle Less Static Visually

--- scripts/lane_objects/obstacles/ghost.gd
+++ scripts/lane_objects/obstacles/ghost.gd
@@ -1,8 +1,23 @@
 extends CollisionObject
 
-# Called when the node enters the scene tree for the first time.
+var cur_offset:float = 0
+var max_offset:float = 0.75
+var multiplier:float = 1
+var move_speed = 0.75
+
 func _ready():
 	type = CollisionType.GHOST
+
+func _process(delta: float) -> void:
+	cur_offset += delta * move_speed * multiplier
+	global_position.y = cur_offset
+	
+	if cur_offset > max_offset:
+		cur_offset = max_offset
+		multiplier *= -1
+	elif cur_offset < 0:
+		cur_offset = 0
+		multiplier *= -1
 
 func apply_effect(player: Node3D) -> void:
 	super.apply_effect(player)
